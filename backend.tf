2b09f381e43de9e51e0f18bf3e466addfc96af07
DerekYZ <79599800+DerekYZ@users.noreply.github.com>
2022-02-16 14:32:54 -0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "DerekZ_tf_backend"
-    storage_account_name = "derekzstorageaccounttf"
+    storage_account_name = "storageaccount4backend"
     container_name       = "con"
     key                  = "key"
   }
