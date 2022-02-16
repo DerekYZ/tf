193426ab8bab10d2eb287c12cf2113e7640d8ed7
DerekYZ <79599800+DerekYZ@users.noreply.github.com>
2022-02-16 14:54:19 -0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "DerekZ_tf_backend"
     storage_account_name = "storageaccount4backend"
     container_name       = "con"
-    key                  = "key"
+    key                  = "DerekZtfkey"
   }
 }
